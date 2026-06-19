f4d5ba723202cd8cae82f9833d093d8b6dd1e1d3
oreo <24416bm@hvhs.school.nz>
2026-06-19 02:44:24 +0000

    Jun 19, 2026, 2:44 AM

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const NAME_FIELD = document.getElementById("nameField");
 
 //Variables
 let userName = "Beatrix";
+let chocStrawberry = "3";
+let chocVanilla = "3";
+let chocChocolate = "3";
+let vanStrawberry = "3";
+let vanVanilla = "3";
+let vanChocolate = "3";
 
 
 //Functions
@@ -20,7 +26,12 @@ function receipt(){
 }
 
 //Output / Main code
-
+displayProduct(chocStrawberry, 3)
+displayProduct(chocVanilla, 3)
+displayProduct(chocChocolate, 3)
+displayProduct(vanStrawberry, 3)
+displayProduct(vanVanilla, 3)
+displayProduct(vanChocolate, 3)
 
 //Console output
 console.log ("Testing pages console")
