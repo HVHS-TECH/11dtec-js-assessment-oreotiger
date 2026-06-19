4f7a82524a0451e9e690ebefcda8f88b7193ea87
oreo <24416bm@hvhs.school.nz>
2026-06-19 02:47:16 +0000

    Jun 19, 2026, 2:47 AM

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const NAME_FIELD = document.getElementById("nameField");
 
 //Variables
 let userName = "Beatrix";
+let userMoney = "5"
 let chocStrawberry = "3";
 let chocVanilla = "3";
 let chocChocolate = "3";
@@ -26,12 +27,20 @@ function receipt(){
 }
 
 //Output / Main code
+change = userMoney - 3;
 displayProduct(chocStrawberry, 3)
 displayProduct(chocVanilla, 3)
 displayProduct(chocChocolate, 3)
 displayProduct(vanStrawberry, 3)
 displayProduct(vanVanilla, 3)
 displayProduct(vanChocolate, 3)
+ if (userMoney >= 3) {
+        calculateChange(userMoney, 3);
+        OUTPUT.innerHTML += "<p>A chocolate bar costs $4. <br> You CAN afford a chocolate bar. <br> You will get $" + change + " change. </p></p> "
+    } else {
+        OUTPUT.innerHTML += "<p>A chocolate bar costs $4. <br> Sorry you CAN'T afford a chocolate bar. <br> You will get no change. </p> "
+    }
+
 
 //Console output
 console.log ("Testing pages console")
