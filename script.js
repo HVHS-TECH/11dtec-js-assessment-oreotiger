872a053ed288849e969eec95e835ab31a0d21e2e
oreo <24416bm@hvhs.school.nz>
2026-06-29 21:42:01 +0000

    Jun 29, 2026, 9:42 PM

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Running script.js");
 
 //Constants
-OUTPUT = document.getElementById ("JavaScriptOutput");
+const OUTPUT = document.getElementById ("JavaScriptOutput");
 const NAME_FIELD = document.getElementById("nameField");
 const MONEY_FIELD = document.getElementById("moneyField");
 
