7ca3f713e64647880d61a96676e1b968aa1f56e9
oreo <24416bm@hvhs.school.nz>
2026-06-29 21:08:14 +0000

    Jun 29, 2026, 9:08 PM

--- script.js
+++ script.js
@@ -3,40 +3,47 @@ console.log("Running script.js")
 //Constants
 const OUTPUT = document.getElementById ("JavaScriptOutput");
 const NAME_FIELD = document.getElementById("nameField");
+const MONEY_FIELD = document.getElementById("moneyField");
 
 //Variables
-let userName = "Beatrix";
-let userMoney = "5"
-let chocStrawberry = "3";
-let chocVanilla = "3";
-let chocChocolate = "3";
-let vanStrawberry = "3";
-let vanVanilla = "3";
-let vanChocolate = "3";
+let userName = String (NAME_FIELD.value);
+let userMoney = Number (MONEY_FIELD.value);
+let chocStrawberry = 3;
+let chocVanilla = 3;
+let chocChocolate = 3;
+let vanStrawberry = 3;
+let vanVanilla = 3;
+let vanChocolate = 3;
 
 
 //Functions
 
-function receipt(){
-    userName = NAME_FIELD.value
-    OUTPUT.innerHTML = userName;
-    change = userMoney - 3;
-    display(chocStrawberry, 3)
-    display(chocVanilla, 3)
-    display(chocChocolate, 3)
-    display(vanStrawberry, 3)
-    display(vanVanilla, 3)
-    display(vanChocolate, 3)
-    if (userMoney >= 3) {
-            calculateChange(userMoney, 3);
-            OUTPUT.innerHTML += "<p>A chocolate bar costs $4. <br> You CAN afford a chocolate bar. <br> You will get $" + change + " change. </p></p> "
-        } else {
-            OUTPUT.innerHTML += "<p>A chocolate bar costs $4. <br> Sorry you CAN'T afford a chocolate bar. <br> You will get no change. </p> "
-        }
+function order(){
+    userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += userName;
 }
 
 function display (_name, _price){
     OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>"
+}
+
+function receipt(){
+    let userMoney = Number (MONEY_FIELD.value);
+    userName = NAME_FIELD.value
+    OUTPUT.innerHTML = userName;
+    change = userMoney - 3;
+    display(chocStrawberry, 3);
+    display(chocVanilla, 3);
+    display(chocChocolate, 3);
+    display(vanStrawberry, 3);
+    display(vanVanilla, 3);
+    display(vanChocolate, 3);
+    if (userMoney >= 3) {
+            calculateChange(userMoney, 3);
+            OUTPUT.innerHTML += "</p>You can afford this order. <br> You will get $" + change + " change. </p> "
+        } else {
+            OUTPUT.innerHTML += "<p>Sorry you can't afford this order. <br> You will get no change. </p> "
+        }
 }
 
 function calculateChange (_money, _price) {
@@ -44,10 +51,7 @@ function calculateChange (_money, _price) {
     return change;
 }
 
-function order(){
-    userName = NAME_FIELD.value;
-    OUTPUT.innerHTML = userName;
-}
+
 
 //Output / Main code
 
