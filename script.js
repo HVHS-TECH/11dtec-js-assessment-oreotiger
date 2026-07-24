6e1622ae278b98277e4bac86a74b4402ae0b470b
oreo <24416bm@hvhs.school.nz>
2026-07-24 08:55:57 +0000

    Jul 24, 2026, 8:55 AM

--- script.js
+++ script.js
@@ -109,13 +109,13 @@ function receipt() {
         OUTPUT.innerHTML += "<p>Your order costs " + order1 + ". </p>"
         OUTPUT.innerHTML += "<p>You have " + userMoney + ".</p>"
         OUTPUT.innerHTML += "<p>You can afford this order. <br> You will get $" + change + " change. </p> ";
-        OUTPUT.innerHTML += "<p>Please rate your experience out of 5. </p>"
+        OUTPUT.innerHTML += "<button onClick = "addRemoveDiv()"><button>"
     } else {
         OUTPUT.innerHTML += "<p>Order for " + userName + ".</p>";
         OUTPUT.innerHTML += "<p>Your order costs " + order1 + ". </p>"
         OUTPUT.innerHTML += "<p>You have " + userMoney + ".</p>"
         OUTPUT.innerHTML += "<p>Sorry you can't afford this order.</p>";
-        OUTPUT.innerHTML += "<p>Please rate your experience out of 5. </p>"
+        OUTPUT.innerHTML += 
     }
 }
 
